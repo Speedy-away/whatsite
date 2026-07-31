da744b108600ff5f9cdaca1d0532f6fd7d31c143
whatwhatboyy <whatwhatboy98@gmail.com>
2026-07-31 08:21:22 +0700

    Fix stacked status badges, add Steam Deck page with Decky Loader, RetroArch logo

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for whatwhatboy.com
-const CACHE_VERSION = 'v3.1.3';
+const CACHE_VERSION = 'v3.1.4';
 const CACHE_NAME = `whatwhatboy-${CACHE_VERSION}`;
 
 const PRECACHE_ASSETS = [
