571d765d8da4ded88aeede7c7615997d218e98e1
whatwhatboyy <159962166+whatwhatboyy@users.noreply.github.com>
2026-08-27 21:26:50 +0700

    Add superhero and Dive mods to RDR2 with screenshots; Rampage screenshot

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for whatwhatboy.com
-const CACHE_VERSION = 'v3.1.8';
+const CACHE_VERSION = 'v3.1.9';
 const CACHE_NAME = `whatwhatboy-${CACHE_VERSION}`;
 
 const PRECACHE_ASSETS = [
